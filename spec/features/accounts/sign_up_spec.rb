a3e1d6227c86f5a66200ba6a3e1510f9d6573f87
Ukaza Perdana <ukaza.putro@jurnal.id>
2017-04-09 11:59:17 +0700

    Added accounts

---
+++ spec/features/accounts/sign_up_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'Accounts' do
+  scenario 'creating an account' do
+    visit root_path
+    # save_and_open_page
+    click_link "Create a new account"
+    fill_in "Name", with: "Test"
+    click_button "Create Account"
+
+    within('.flash_notice') do
+      success_message = "Your account has been created."
+      expect(page).to have_content(success_message)
+    end
+  end
+end
